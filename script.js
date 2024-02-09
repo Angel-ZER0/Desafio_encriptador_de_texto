e5c51ff04837922905a841ee82f202e41382c3b5
Angel-ZER0 <cora.angelandres@gmail.com>
2024-02-08 22:13:02 -0600

    añadiendo funcíon que reconoce caracteres invalidos e interrumpe la ejecucuión del encriptado/desencritado

--- script.js
+++ script.js
@@ -34,6 +34,9 @@ CONTENEDOR_BOTON_1.appendChild(BOTON_DESENCRIPTAR);
 /*/*Creando botón que llamará a la función que copirá el texto
 del párrado encriptado y se le la añade la clase .contenedorBoton
 a su contenedor acomodarlo dentro del mismo*/
+//Creacion párrafo de aviso
+const CARACTER_INVALIDO = document.createElement("p");
+CARACTER_INVALIDO.className = "advertencia";
 const BOTON_COPIAR = document.createElement("button");
 BOTON_COPIAR.textContent = "Copiar texto";
 const CONTENEDOR_BOTON_2 = document.createElement("div");
@@ -60,13 +63,85 @@ const VOCALES = Object.freeze ({
     ufat: "u"
 });
 
+//objeto que contiene los caracteres válidos para la encriptación
+const ALFABETO = Object.freeze({
+  a: 'a',
+  b: 'b',
+  c: 'c',
+  d: 'd',
+  e: 'e',
+  f: 'f',
+  g: 'g',
+  h: 'h',
+  i: 'i',
+  j: 'j',
+  k: 'k',
+  l: 'l',
+  m: 'm',
+  n: 'n',
+  ñ: 'ñ',
+  o: 'o',
+  p: 'p',
+  q: 'q',
+  r: 'r',
+  s: 's',
+  t: 't',
+  u: 'u',
+  v: 'v',
+  w: 'w',
+  x: 'x',
+  y: 'y',
+  z: 'z',
+  "1": "1",
+  "2": "2",
+  "3": "3",
+  "4": "4",
+  "5": "5",
+  "6": "6",
+  "7": "7",
+  "8": "8",
+  "9": "9",
+  "0": "0",
+  " ": " ",
+  "¡": "¡",
+  "!": "!",
+  "¿": "¿",
+  "?": "?",
+  "'": "'",
+  '"': '"',
+  ",": ",",
+  ".": ".",
+  "\n": "\n",
+  ":": ":",
+  "(": "(",
+  ")": ")"
+});
+
 //Función que controla el texto y aparicion del botón para copiar el texto cifrado/descifrado
 function aparicionBoton () {
     BOTON_COPIAR.textContent = "Copiar texto";
-    if (ENCRIPTADOR.value.length > 0) {
-        CONSOLA_SALIDA.appendChild(CONTENEDOR_BOTON_2);
-    } else {
+    if (ENCRIPTADOR.value.length === 0 && [...CONSOLA_SALIDA.childNodes].includes(CONTENEDOR_BOTON_2) || TEXTO_ENCRIPTADO.textContent === "Eliminar caracteres inválidos antes de continuar." && [...CONSOLA_SALIDA.childNodes].includes(CONTENEDOR_BOTON_2)) {
         CONSOLA_SALIDA.removeChild(CONTENEDOR_BOTON_2);
+    } else if (ENCRIPTADOR.value.length > 0 && TEXTO_ENCRIPTADO.textContent !== "Eliminar caracteres inválidos antes de continuar.") {
+        CONSOLA_SALIDA.appendChild(CONTENEDOR_BOTON_2);
+    }
+}
+
+/*Función que verifica que el elemento ENCRIPTADOR no contenga caracteres no
+ deseados en el texto de salida*/
+function verifivadorCaracteres() {
+    let arregloTexto = ENCRIPTADOR.value.split("");
+    let arregloCaracteresInvalidos = [];
+    for (let caracter of arregloTexto) {
+        if (ALFABETO.hasOwnProperty(caracter) === false) {
+            arregloCaracteresInvalidos.push(caracter);
+        }
+    }
+    if (arregloCaracteresInvalidos.length > 0) {
+        CARACTER_INVALIDO.innerHTML = "Caracter inválido introducido, favor de eliminar el/los siguiente(s) caracter(es): <span class='azul'>" + arregloCaracteresInvalidos.join(" ").toString() + "</span>";
+        CONSOLA_ENTRADA.appendChild(CARACTER_INVALIDO);
+    } else {
+        CARACTER_INVALIDO.remove();
     }
 }
 
@@ -75,14 +150,22 @@ function aparicionBoton () {
  buscando las vocales para reemplazarlas según sea el caso con su contraparte cifrada declarada
  en el objeto PALABRAS y arroja el resultado unido de nuevo y transformado a una cadena en el
  elemento TEXTO_ENCRIPTADO*/
-function encriptarTexto() {
-    let arregloTexto = ENCRIPTADOR.value.split("");
-    for (let caracter of arregloTexto) {
-        if (PALABRAS.hasOwnProperty(caracter)) {
-            arregloTexto.splice(arregloTexto.indexOf(caracter), 1, PALABRAS[caracter]);
+function encriptarTexto () {
+    //llamada a la función verifivadorCaracteres
+    verifivadorCaracteres();
+    //condicional que no permite visualizar texto cifrado si este el elemento ENCRIPTADOR contiene caracteres inválidos
+    if ([...CONSOLA_ENTRADA.childNodes].includes(CARACTER_INVALIDO)) {
+        TEXTO_ENCRIPTADO.innerText = "Eliminar caracteres inválidos antes de continuar.";
+    //condicional que a permite la ejecución del encriptado de manera correcta
+    } else {
+        let arregloTexto = ENCRIPTADOR.value.split("");
+        for (let caracter of arregloTexto) {
+            if (PALABRAS.hasOwnProperty(caracter)) {
+                arregloTexto.splice(arregloTexto.indexOf(caracter), 1, PALABRAS[caracter]);
+            }
         }
+        TEXTO_ENCRIPTADO.innerHTML = arregloTexto.join("").toString();
     }
-    TEXTO_ENCRIPTADO.innerHTML = arregloTexto.join("").toString();
     aparicionBoton();
 }
 
@@ -90,11 +173,16 @@ function encriptarTexto() {
  meditante un ciclo y el objeto VOCALES para después arrojar el resultado al elemento TEXTO_ENCRIPTADO*/
 function reemplazarCifrados() {
     let textoDescifrado = ENCRIPTADOR.value;
-    for (const vocal in VOCALES) {
-        const regex = new RegExp(vocal, 'g');
-        textoDescifrado = textoDescifrado.replace(regex, VOCALES[vocal]);
+    //condicional que no permite visualizar texto descifrado si este el elemento ENCRIPTADOR contiene caracteres inválidos
+    if ([...CONSOLA_ENTRADA.childNodes].includes(CARACTER_INVALIDO)) {
+        TEXTO_ENCRIPTADO.innerText = "Eliminar caracteres inválidos antes de continuar.";
+    } else {
+        for (const vocal in VOCALES) {
+            const regex = new RegExp(vocal, 'g');
+            textoDescifrado = textoDescifrado.replace(regex, VOCALES[vocal]);
+        }
+        TEXTO_ENCRIPTADO.innerHTML = textoDescifrado;
     }
-    TEXTO_ENCRIPTADO.innerHTML = textoDescifrado;
     aparicionBoton();
 }
 
@@ -102,6 +190,7 @@ function reemplazarCifrados() {
 MODOS.addEventListener("click", () => {
     //Si el botón clickeado es el ubicado en ACTIVAR_ENCRIPTADOR
     if (event.target === ACTIVAR_ENCRIPTADOR) {
+        ENCRIPTADOR.removeEventListener("keyup", verifivadorCaracteres);
         //Coloca la clase .activo a este botón
         ACTIVAR_ENCRIPTADOR.className = "activo";
         //Borra el texto dentro del elemento ENCRIPTADOR
@@ -121,9 +210,11 @@ MODOS.addEventListener("click", () => {
         if (ACTIVAR_DESENCRIPTADOR.classList.contains("activo")) {
             ACTIVAR_DESENCRIPTADOR.classList.toggle("activo");
         }
-        //Condicinal que remueve el elemento CONTENEDOR_BOTON_1 si la CONSOLA_ENTRADA cuenta con el
-        if ([...CONSOLA_ENTRADA.childNodes].includes(CONTENEDOR_BOTON_1)) {
+        /*Condicinal que remueve el elemento CONTENEDOR_BOTON_1 y el parráfo CARACTER_INVALIDO
+        de la CONSOLA_ENTRADA si cuenta con el*/
+        if ([...CONSOLA_ENTRADA.childNodes].includes(CONTENEDOR_BOTON_1) || [...CONSOLA_ENTRADA.childNodes].includes(CARACTER_INVALIDO)) {
             CONSOLA_ENTRADA.removeChild(CONTENEDOR_BOTON_1);
+            CONSOLA_ENTRADA.removeChild(CARACTER_INVALIDO);
         }
     //condicional de la función en caso que se clickeé el BOTON_DESENCRIPTAR
     } else if (event.target === ACTIVAR_DESENCRIPTADOR) {
@@ -142,9 +233,15 @@ MODOS.addEventListener("click", () => {
         /*añade un eventListener al botón BOTON_DESENCRIPTAR de tipo click que llama
         la función para desencriptar el texto escrito en ENCRIPTADOR*/
         BOTON_DESENCRIPTAR.addEventListener("click", reemplazarCifrados);
+        //añade un eventListener de tipo keyup al elemento encriptador
+        ENCRIPTADOR.addEventListener("keyup", verifivadorCaracteres);
         //Condicinal que elimina el contenedor del botón copiar en caso de tenerlo
         if ([...CONSOLA_SALIDA.childNodes].includes(CONTENEDOR_BOTON_2)) {
             CONSOLA_SALIDA.removeChild(CONTENEDOR_BOTON_2);
+        }
+        //condicional que remueve el párrafo que indica los caracteres inválidos
+        if ([...CONSOLA_ENTRADA.childNodes].includes(CARACTER_INVALIDO)) {
+            CONSOLA_ENTRADA.removeChild(CARACTER_INVALIDO);
         }
         //condicional que desactiva la clase .activo del botón ACTIVAR_ENCRIPTADOR
         if (ACTIVAR_ENCRIPTADOR.classList.contains("activo")) {
